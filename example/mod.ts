6818ea72810c6b8aef8bc272f2fc846763f51552
uki00a <uki00a@gmail.com>
2024-07-01 01:30:53 +0900

    fix

--- example/mod.ts
+++ example/mod.ts
@@ -2,7 +2,6 @@ import { OakAdapter } from "@uki00a/nestjs-platform-oak";
 import { NestFactory } from "@nestjs/core";
 import type { INestApplication } from "@nestjs/common";
 import { Controller, Get, Module } from "@nestjs/common";
-import { Application } from "@oak/oak";
 
 @Controller("/api")
 class ApiController {
