03facd5ddcc66b821d923a520f384fd1cba1d3c1
uki00a <uki00a@gmail.com>
2024-09-04 23:41:35 +0900

    fix

--- packages/nestjs-platform-oak/src/decorators.ts
+++ packages/nestjs-platform-oak/src/decorators.ts
@@ -1,7 +1,7 @@
 import type { ExecutionContext } from "@nestjs/common";
 import { createParamDecorator } from "@nestjs/common";
 
-import { Request } from "@oak/oak";
+import type { Request } from "@oak/oak";
 
 /**
  * NOTE: The `@Body()` decorator provided by `@nestjs/common` injects Oak's {@linkcode Request.body}. Use this decorator if you want to inject the request body as JSON, .
