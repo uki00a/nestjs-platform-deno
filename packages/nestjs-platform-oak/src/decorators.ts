e6f6da12d5ef63620959cede59b15ea909988adc
Yuki Tanaka <uki00a@gmail.com>
2024-09-06 00:05:58 +0900

    chore: introduce Deno Workspaces (#25)

--- packages/nestjs-platform-oak/src/decorators.ts
+++ packages/nestjs-platform-oak/src/decorators.ts
@@ -6,7 +6,7 @@ import type { Request } from "@oak/oak";
 /**
  * NOTE: The `@Body()` decorator provided by `@nestjs/common` injects Oak's {@linkcode Request.body}. Use this decorator if you want to inject the request body as JSON.
  */
-export const JsonBody = createParamDecorator(
+export const JsonBody: ParameterDecorator = createParamDecorator(
   async (_data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<Request>();
     const body = await request.body.json();
