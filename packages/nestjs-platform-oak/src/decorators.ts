02dd462121bfb26d97814da4507f4d5a38e57022
uki00a <uki00a@gmail.com>
2024-09-06 00:09:47 +0900

    fix: improve type of `@JsonBody()`

--- packages/nestjs-platform-oak/src/decorators.ts
+++ packages/nestjs-platform-oak/src/decorators.ts
@@ -1,4 +1,4 @@
-import type { ExecutionContext } from "@nestjs/common";
+import type { ExecutionContext, PipeTransform, Type } from "@nestjs/common";
 import { createParamDecorator } from "@nestjs/common";
 
 import type { Request } from "@oak/oak";
@@ -6,7 +6,9 @@ import type { Request } from "@oak/oak";
 /**
  * NOTE: The `@Body()` decorator provided by `@nestjs/common` injects Oak's {@linkcode Request.body}. Use this decorator if you want to inject the request body as JSON.
  */
-export const JsonBody: ParameterDecorator = createParamDecorator(
+export const JsonBody: (
+  ...dataOrPipes: (Type<PipeTransform> | PipeTransform | unknown)[]
+) => ParameterDecorator = createParamDecorator(
   async (_data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<Request>();
     const body = await request.body.json();
