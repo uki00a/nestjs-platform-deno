b535d3fb4f6806fecdbe6cd91de2852e26dafccb
uki00a <uki00a@gmail.com>
2024-09-16 02:21:25 +0900

    feat(nestjs-denokv): support a default provider

--- packages/nestjs-denokv/src/index.ts
+++ packages/nestjs-denokv/src/index.ts
@@ -1,3 +1,4 @@
 export { DenoKvModule } from "./denokv.module.ts";
 export type { DenoKvModuleOptions } from "./denokv.module.ts";
 export { InjectKv } from "./denokv.decorator.ts";
+export { DenoKvRef } from "./denokv.constants.ts";
