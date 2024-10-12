2fa86f302aac5762cdcc05767ec4258e9f15ad3f
uki00a <uki00a@gmail.com>
2024-10-12 16:44:19 +0900

    fix: lint error

--- packages/nestjs-platform-hono/src/nest-hono-application.interface.ts
+++ packages/nestjs-platform-hono/src/nest-hono-application.interface.ts
@@ -2,6 +2,12 @@ import type { INestApplication } from "@nestjs/common";
 import type { ServeStaticOptions } from "@hono/hono/serve-static";
 
 /**
+ * @internal
+ * @see {@linkcode ServeStaticOptions}
+ */
+type HonoServeStaticOptions = ServeStaticOptions;
+
+/**
  * {@link https://github.com/nestjs/nest/blob/v10.4.4/packages/core/nest-application.ts}
  */
 export interface NestHonoApplication<
@@ -10,5 +16,5 @@ export interface NestHonoApplication<
   /**
    * Registers static assets under a {@linkcode path}.
    */
-  useStaticAssets(path: string, options?: ServeStaticOptions): this;
+  useStaticAssets(path: string, options: HonoServeStaticOptions): this;
 }
